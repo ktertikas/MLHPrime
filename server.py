e649f3c1be5cc893a93e19b2c5cd94ce45a000d8
Marios Siganos <marios.siganos@gmail.com>
2017-04-02 04:22:42 +0100

    fix syntax again

--- server.py
+++ server.py
@@ -105,7 +105,7 @@ handlers = [
 settings = dict(
     template_path=os.path.join(os.path.dirname(__file__), "frontend"),
     static_path=os.path.join(os.path.dirname(__file__), "static"),
-    cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo="",
+    cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
     login_url="/login",
 )
 
