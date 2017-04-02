48f03b2644b81b001d0e50ff1a87bfc5035575a6
Marios Siganos <marios.siganos@gmail.com>
2017-04-02 12:42:28 +0100

    fix debug

--- server.py
+++ server.py
@@ -168,7 +168,7 @@ settings = dict(
     static_path=os.path.join(os.path.dirname(__file__), "static"),
     cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
     login_url="/login",
-    "debug" : True,
+    "debug" = True,
 )
 
 application = tornado.web.Application(handlers, **settings)
